cf6d77c22df9c072dc2902439df44b66df275434
kaspervh <kaspervhauschildt@protonmail.com>
2026-08-28 14:46:02 +0200

    made some progress

--- download-manager.sh
+++ download-manager.sh
@@ -1,3 +1,15 @@
-artists=$(jq -r 'keys[]' download-list.json)
+mapfile -t artists < <(jq -r 'keys[]' download-list.json)
 
-echo "artists"
+for i in ${artists[@]}; do 
+	cd ~/Music/
+	if [ -d "$i" ]; then
+		echo directory $i exists
+	else
+		mkdir $i
+		cd $i
+		mapfile -t albums < <(jq -r --args "$i" '.[artist][] | keys[]' download-list.json)
+
+		echo " $albums[@] "
+	fi
+	
+done
