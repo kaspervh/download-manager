6d49ac6286aa59846f2c49101e22c4fa8a373b65
kaspervh <kaspervhauschildt@protonmail.com>
2026-09-02 16:19:35 +0200

    working on the list

--- download-manager.sh
+++ download-manager.sh
@@ -1,6 +1,19 @@
 mapfile -t artists < <(jq -r 'keys[]' download-list.json)
 json_file="$PWD"
 
+if ! command -v jq >/dev/null 2>&1; then
+	echo ERROR jq not installed, please insall it with the following commands
+	echo sudo apt update
+	echo sudo apt install -y jq
+	exit 1
+fi 
+
+if ! command -v yt-dlp >/dev/null 2>&1; then
+	echo ERROR yt-dlp not installed, please install it with the following commands
+	echo sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+	echo sudo chmod a+rx /usr/local/bin/yt-dlp
+fi
+
 
 #loops over each artist in order to create folders  
 for i in ${artists[@]}; do 
